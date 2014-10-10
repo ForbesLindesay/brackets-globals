ForbesLindesay/brackets-globals

commit 0f6dccc90fb94271b734fd1ac3a50130509fce9d

Fix Deprecation Warning in upcoming 0.44 release

main.js
@@ -10,9 +10,10 @@ define(function (require, exports, module) {
   var Document = brackets.getModule("document/Document");
   var documentManager = brackets.getModule("document/DocumentManager");
   var editorManager = brackets.getModule("editor/EditorManager");
+  var mainViewManager = brackets.getModule("view/MainViewManager");
 
   var refreshing = false;
-  $(documentManager).on('currentDocumentChange', function (e, newDocument, oldDocument) {
+  $(mainViewManager).on('currentFileChange', function (e, newDocument, oldDocument) {
     try {
       refreshing = false;
       if (oldDocument) {

commit f9d0fb35edddd9be1579e1b1a75b79c19f37b5eb

Fix bug introduced in 0f6dccc90fb94271b734fd1ac3a50130509fce9d that made the module not work at all

main.js
@@ -13,7 +13,7 @@ define(function (require, exports, module) {
   var mainViewManager = brackets.getModule("view/MainViewManager");
 
   var refreshing = false;
-  $(mainViewManager).on('currentFileChange', function (e, newDocument, oldDocument) {
+  $(editorManager).on('activeEditorChange', function (e, newDocument, oldDocument) {
     try {
       refreshing = false;
       if (oldDocument) {